a092c12b3abb49c2b8e29da8e0b925f7f022502e
Stylix58 <lateman-jpeg@outlook.fr>
2021-04-17 22:37:55 +0200

    commit before switch to raylen's pc
    Co-authored-by: Raylen <ausername-1@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+//TODO: Install nodemon
 const Discord = require('discord.js')
+const client = new Discord.Client();
+//TODO Install FS, ENMAP and create command handler *DONE*
+const fs = require("fs");
+const config = require("./config.json");
+
+client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+
+client.login(process.env.TOKEN)
+// what, we install wsl 
+// should i install wsl?
